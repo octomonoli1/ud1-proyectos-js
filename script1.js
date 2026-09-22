28ccecf448b2158ad554d7ccae1bc3d74c64c36b
octomonoli1 <octavio.mo90@gmail.com>
2026-09-22 11:15:56 +0200

    mas ejemplos

--- script1.js
+++ script1.js
@@ -1,18 +1,70 @@
-//Ejemplo 1
+//Ejemplo 1: Hola mundo JS
 console.log("Hola mundo");
 
-//Ejemplo 2
-var nombre1 = "Octavio";
+//Ejemplo 2: Saluda nombre desde variable 
+const nombre1 = "Octavio";
 console.log(`Hola ${nombre1}`);
 
-//Ejemplo 3
-var nombre2 = window.prompt("Introduce tu nombre: ");
+//Ejemplo 3: Saluda nombre con entrada
+const nombre2 = window.prompt("Introduce tu nombre: ");
 console.log(`Hola ${nombre2}`);
 
-//Ejemplo 4
-var edad = window.prompt("Introduce tu edad: ");
-if(edad >= 18){
+//Ejemplo 4: Primer if
+const edad = window.prompt("Introduce tu edad: ");
+if (edad >= 18) {
     console.log("Eres mayor de edad");
-}else{
+} else {
     console.log("Eres menor de edad");
 }
+
+//Ejemplo 5: Defincion de variables segun su ámbito
+
+//const: constante (su valor no varía)
+//let: ambito de bloque
+//var: ambio de funcion
+
+//Definicion funcion
+function calcula_edad() {
+    const edad2 = window.prompt("Introduce tu edad: ");
+    if (edad2 >= 18) {
+        var mensaje = "Eres mayor de edad";
+    } else {
+        mensaje = "Eres menor de edad";
+    }
+    console.log(mensaje);
+}
+
+//Llamada a la funcion
+calcula_edad();
+
+//Ejemplo 6
+let auxiliar;
+auxiliar = 10;
+console.log(auxiliar);
+
+auxiliar = "Mi casa es roja";
+console.log(auxiliar);
+
+auxiliar = true;
+console.log(auxiliar);
+
+auxiliar = 1432.12;
+console.log(auxiliar);
+
+
+//Ejemplo 7: Primer bucle
+function potencia(base, exponente) {
+
+    let resultado = 1;
+
+    while(exponente > 0){
+        resultado *= base
+        exponente --;
+    }
+
+    return resultado;
+}
+
+let base = 2;
+let exponente = 3;
+console.log(`El resultado de ${base}^${exponente} es:${potencia(base, exponente)}`);
