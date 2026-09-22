2e084bbcd1330c724c19bae4de454004afb48f05
octomonoli1 <octavio.mo90@gmail.com>
2026-09-22 10:41:43 +0200

    primeros tres ejemplos

---
+++ script1.js
@@ -0,0 +1,18 @@
+//Ejemplo 1
+console.log("Hola mundo");
+
+//Ejemplo 2
+var nombre1 = "Octavio";
+console.log(`Hola ${nombre1}`);
+
+//Ejemplo 3
+var nombre2 = window.prompt("Introduce tu nombre: ");
+console.log(`Hola ${nombre2}`);
+
+//Ejemplo 4
+var edad = window.prompt("Introduce tu edad: ");
+if(edad >= 18){
+    console.log("Eres mayor de edad");
+}else{
+    console.log("Eres menor de edad");
+}
